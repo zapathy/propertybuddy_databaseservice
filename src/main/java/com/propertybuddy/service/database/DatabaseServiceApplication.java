c775f77cc7f0edd0d737a950d257beab653cdcd9
zapathy <apathy.zsombor@gmail.com>
2019-05-03 23:20:01 +0200

    first commit

---
+++ src/main/java/com/propertybuddy/service/database/DatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.propertybuddy.service.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseServiceApplication.class, args);
+    }
+
+}
