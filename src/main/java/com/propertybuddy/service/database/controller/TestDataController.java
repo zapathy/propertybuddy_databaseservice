cd48154301ee207c247ca5a710f727af7d192cc6
zapathy <apathy.zsombor@gmail.com>
2019-05-08 14:32:53 +0200

    second test property

--- src/main/java/com/propertybuddy/service/database/controller/TestDataController.java
+++ src/main/java/com/propertybuddy/service/database/controller/TestDataController.java
@@ -1,5 +1,6 @@
 package com.propertybuddy.service.database.controller;
 
+import com.propertybuddy.service.database.model.MessageObject;
 import com.propertybuddy.service.database.model.Price;
 import com.propertybuddy.service.database.model.Property;
 import com.propertybuddy.service.database.service.PropertyService;
@@ -7,22 +8,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Arrays;
 
 @RestController
-@RequestMapping("/testdata")
+@RequestMapping(value = "/testdata")
 @CrossOrigin
 public class TestDataController {
 
     @Autowired
     PropertyService propertyService;
 
+    @CrossOrigin
     @PostMapping("/createall")
-    public ResponseEntity<String> createTestData() {
+    public ResponseEntity<MessageObject> createTestData() {
+        propertyService.deleteAll();
         propertyService.add(
                 Property.builder()
                         .district(9)
@@ -43,8 +49,28 @@ public class TestDataController {
                         ))
                         .build()
         );
+        propertyService.add(
+                Property.builder()
+                        .district(7)
+                        .street("Erzsébet körút")
+                        .priceHistory(Arrays.asList(
+                                Price.builder()
+                                        .date(LocalDate.of(2018,6,13))
+                                        .price(new BigDecimal(50000))
+                                        .build(),
+                                Price.builder()
+                                        .date(LocalDate.of(2018,9,21))
+                                        .price(new BigDecimal(55000))
+                                        .build(),
+                                Price.builder()
+                                        .date(LocalDate.of(2019,1,5))
+                                        .price(new BigDecimal(54000))
+                                        .build()
+                        ))
+                        .build()
+        );
 
-        return new ResponseEntity<>("Added test data", new HttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(new MessageObject("Added test data"), new HttpHeaders(), HttpStatus.OK);
     }
 
 }
