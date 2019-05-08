899c9715abb9711e8eec0044246c2e873b7056b5
zapathy <apathy.zsombor@gmail.com>
2019-05-08 14:32:35 +0200

    added necessary POJO classes

---
+++ src/main/java/com/propertybuddy/service/database/model/MessageObject.java
@@ -0,0 +1,10 @@
+package com.propertybuddy.service.database.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageObject {
+    private String message;
+}
