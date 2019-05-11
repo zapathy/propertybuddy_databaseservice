a08eca148afad93800812f3bbb75837ac9e06eba
zapathy <apathy.zsombor@gmail.com>
2019-05-11 09:38:46 +0200

    added pojo class for RequestBody input

---
+++ src/main/java/com/propertybuddy/service/database/model/PropertyInput.java
@@ -0,0 +1,28 @@
+package com.propertybuddy.service.database.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PropertyInput {
+
+    private String name;
+    private Integer district;
+    private Integer bedrooms;
+    private Integer size;
+    private String floor;
+    private Boolean garage;
+    private String view;
+    private String streetsuffix;
+    private String streetname;
+    private BigInteger pricehuf;
+    private BigInteger priceeur;
+
+}
