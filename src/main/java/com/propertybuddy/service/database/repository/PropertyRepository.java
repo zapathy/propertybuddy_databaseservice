7c8ee7eb6edef1c9c902821c40b21d9b0e635306
zapathy <apathy.zsombor@gmail.com>
2019-05-20 09:23:18 +0200

    remove imports

--- src/main/java/com/propertybuddy/service/database/repository/PropertyRepository.java
+++ src/main/java/com/propertybuddy/service/database/repository/PropertyRepository.java
@@ -1,8 +1,12 @@
 package com.propertybuddy.service.database.repository;
 
+import com.propertybuddy.service.database.model.Price;
 import com.propertybuddy.service.database.model.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PropertyRepository  extends JpaRepository<Property, Long> {
-
 }
